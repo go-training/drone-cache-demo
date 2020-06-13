c968a1a50e48414ff87de8e2059bcace9e06c8d0
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-06-13 18:04:02 +0800

    chore: add makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,25 @@
+SERVICE               ?= demo
+GOBUILD               := go build
+GOINSTALL             := go install
+GOMOD                 := go mod
+VCS_REF               := $(strip $(shell [ -d .git ] && git rev-parse HEAD))
+VERSION               := $(strip $(shell [ -d .git ] && git describe --abbrev=0))
+BUILD_DATE            := $(shell date -u +"%Y-%m-%dT%H:%M:%S%Z")
+LDFLAGS               := '-s -w -X main.version=$(VERSION) -X main.commit=$(VCS_REF) -X main.date=$(BUILD_DATE)'
+TAGS                  := netgo
+GOFILES               := $(shell find . -name "*.go" -type f)
 
 all: build
 
-build:
-	go build -v -a -o demo .
+.PHONY: vendor
+vendor:
+	$(GOMOD) tidy
+	$(GOMOD) vendor -v
+
+build: $(SERVICE)
+
+$(SERVICE): $(GOFILES)
+	$(GOBUILD) -v -a -ldflags $(LDFLAGS) -tags $(TAGS) -o demo .
 
 docker:
 	docker build --progress=plain -t appleboy/docker-demo -f Dockerfile .
--- main.go
+++ main.go
@@ -5,25 +5,31 @@ import (
 
 	"drone-cache/bar"
 	"drone-cache/foo"
+
+	"github.com/gin-gonic/gin"
 )
 
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("Hello World!"))
-}
-
-func fooHandler(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte(foo.Foo()))
-}
-
-func barHandler(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte(bar.Bar()))
-}
-
 func main() {
-	r := http.NewServeMux()
-	r.HandleFunc("/hello", helloHandler)
-	r.HandleFunc("/foo", fooHandler)
-	r.HandleFunc("/bar", barHandler)
-
-	http.ListenAndServe(":8080", r)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/ping2", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong2",
+		})
+	})
+	r.GET("/ping100", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": foo.Foo(),
+		})
+	})
+	r.GET("/ping101", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": bar.Bar(),
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
